01e14cdea82a227a54c1d2395de419646716d9e5
junjimoriyama <pero197030@icloud.com>
2025-07-20 09:21:36 +0900

    vike.config.tsファイル追加

---
+++ vike.config.ts
@@ -0,0 +1,12 @@
+// vike.config.ts
+const config = {
+  pageFiles: {
+    include: [
+      'src/pages/**/*.+Page.tsx',
+      'src/pages/**/*.+config.ts',
+      'src/pages/**/*.+Layout.tsx',
+    ],
+  },
+};
+
+export default config;
